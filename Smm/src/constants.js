5e96c4c3858f8e009b68d49032e2187ae33aaff8
Anim3sh <76643531+Anim3shSinha@users.noreply.github.com>
2023-12-03 18:42:11 +0530

    ff

--- Smm/src/constants.js
+++
@@ -1,9 +0,0 @@
-const CONST = {
-  // server:"https://smm-serv.onrender.com"
-  // server: "http://localhost:5000",
-  // server:"https://34.238.120.92:8080"
-  // server: "https://one.digtl.biz:8443",
-  server: "http://localhost:8080",
-};
-
-export { CONST };
